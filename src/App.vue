668add275c3c0c80ab7bf4ba8888a3686d61926e
danelian <141317269+danelian@users.noreply.github.com>
2024-03-04 16:57:07 +0400

    Footer - desktop & adaptation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
@@ -8,7 +9,7 @@ import Header from './components/Header.vue'
 
   <RouterView />
 
-  
+  <Footer />
 </template>
 
 <style scoped>
