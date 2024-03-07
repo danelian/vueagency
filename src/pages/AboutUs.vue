95cc44b98ea9150f2f188ecc9ce7b5a14e46b5c2
danelian <141317269+danelian@users.noreply.github.com>
2024-03-08 01:20:57 +0400

    section about-benefits - desktop & adaptation

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -4,6 +4,7 @@ import AboutHero from '@/components/blocks/AboutHero.vue'
 import WhoWeAre from '@/components/blocks/WhoWeAre.vue'
 import AboutProcess from '@/components/blocks/AboutProcess.vue'
 import AboutInfo from '@/components/blocks/AboutInfo.vue'
+import AboutBenefits from '@/components/blocks/AboutBenefits.vue'
 import Team from '@/components/blocks/Team.vue';
 
 export default {
@@ -12,6 +13,7 @@ export default {
     WhoWeAre,
     AboutProcess,
     AboutInfo,
+    AboutBenefits,
     Team,
   },
   setup() {
@@ -28,5 +30,6 @@ export default {
   <WhoWeAre />
   <AboutProcess />
   <AboutInfo />
+  <AboutBenefits />
   <Team :teamMembers="teamMembers" />
 </template>
