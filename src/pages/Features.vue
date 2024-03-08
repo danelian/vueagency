2e8e9d28d51db47e8c097196b3cd2bcb34b8b27d
danelian <141317269+danelian@users.noreply.github.com>
2024-03-08 16:16:23 +0400

    page features - completed

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -1,13 +1,19 @@
 <script>
 import FeaturesHero from '@/components/blocks/FeaturesHero.vue'
+import AboutBenefits from '@/components/blocks/AboutBenefits.vue'
+import Faq from '@/components/blocks/Faq.vue'
 
 export default {
   components: {
-    FeaturesHero
+    FeaturesHero,
+    AboutBenefits,
+    Faq,
   }
 }
 </script>
 
 <template>
   <FeaturesHero />
+  <AboutBenefits />
+  <Faq />
 </template>
