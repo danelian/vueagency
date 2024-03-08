80056b639ce0ed384e8b46a9f18b985ba9f36c71
danelian <141317269+danelian@users.noreply.github.com>
2024-03-08 16:36:41 +0400

    section portfolio-hero - desktop & adaptation

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,7 +1,21 @@
-<script setup>
+<script>
+import { useStore } from '../store';
+import PortfolioHero from '@/components/blocks/PortfolioHero.vue'
 
+export default {
+  components: {
+    PortfolioHero,
+  },
+  setup() {
+    const store = useStore();
+
+    return {
+      cases: store.cases,
+    };
+  },
+};
 </script>
 
 <template>
-
+  <PortfolioHero />
 </template>
