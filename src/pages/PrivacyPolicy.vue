d5120ca1deb5f0d4cf9b170b4687e0e84ce5941c
danelian <141317269+danelian@users.noreply.github.com>
2024-03-09 20:42:16 +0400

    page privacy policy

--- src/pages/PrivacyPolicy.vue
+++ src/pages/PrivacyPolicy.vue
@@ -1,7 +1,63 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-
+  <section class="privacy-policy">
+    <div class="container">
+      <div class="heading">
+        <h1 class="title-h2">Privacy Policy</h1>
+        <p>
+          When you’re ready to go beyond prototyping in Figma, Webflow’s ready to help you bring your
+        </p>
+      </div>
+      <h2 class="title-h3">Lorem ipsum dolor.</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident.
+      </p>
+      <p>
+        Sunt in culpa qui officia deserunt mollit anim id est laborum. Ut enim ad minim veniam, quis
+        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+        anim id est laborum.
+      </p>
+      <h3 class="title-h5">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident. Dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        exercitation ullamco laboris.
+      </p>
+    </div>
+  </section>
 </template>
+
+<style lang="scss" scoped>
+.heading {
+  text-align: center;
+  margin: 0 auto 60px auto;
+}
+p {
+  margin-bottom: 24px;
+}
+ul,
+ol {
+  li {
+    &:not(:last-child) {
+      margin-bottom: 12px;
+    }
+  }
+}
+
+@media screen and (max-width: 576px) {
+  .heading {
+    margin-bottom: 40px;
+  }
+}
+</style>
